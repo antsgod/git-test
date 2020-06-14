cfd512efd4c02c39e754e4abe9e2deb2e86e4ea6
antsgod <antsgod30329398@163.com>
2020-06-14 23:13:16 +0800

    提交到本地dev分支

---
+++ java/src/comanche/Second.java
@@ -0,0 +1,15 @@
+package comanche;
+
+public class Second {
+    public static void main(String[] args) {
+        System.out.println("hello git");
+    }
+
+    public void test(){
+        System.out.println("this is a test case");
+    }
+
+    public void test1(){
+        System.out.println("this is a test case");
+    }
+}
