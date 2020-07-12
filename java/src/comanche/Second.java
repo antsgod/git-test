b99f0a79817691b0a31161bf9f6711873e871c98
antsgod <antsgod30329398@163.com>
2020-07-12 10:37:51 +0800

    验证冲突

--- java/src/comanche/Second.java
+++ java/src/comanche/Second.java
@@ -4,6 +4,7 @@ public class Second {
 
     public void test(int i,String param,double d){
         System.out.println(param);
+        System.out.println(d);
         System.out.println(i);
     }
 }
